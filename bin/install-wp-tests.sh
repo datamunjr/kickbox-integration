91cd9e2074526d939f3f4bdcfe9670c85e11436a
Rafi Yagudin <rafi.yagudin@sunrun.com>
2025-09-26 22:25:11 -0600

    Unit tests!

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -204,6 +204,13 @@ install_woocommerce() {
 		echo "Visit: https://wp-cli.org/#installing"
 		return 1
 	fi
+
+	# Install WordPress core and WooCommerce using WP-CLI
+	cd "$WP_CORE_DIR"
+
+  #	Export the core dir so we can use it for unit tests
+  echo "WP_CORE_DIR is $WP_CORE_DIR"
+	export WP_CORE_DIR=$WP_CORE_DIR
 	
 	# Install WooCommerce
 	if [[ $WC_VERSION == 'latest' ]]; then
